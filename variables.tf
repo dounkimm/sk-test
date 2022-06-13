18da816d16aa7d241e6bf526f2482e6e36d1a782
dounkimm <98010656+dounkimm@users.noreply.github.com>
2022-06-13 15:45:45 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -76,10 +76,28 @@ description = "가상머신 Memory 사이즈"
 default = "2048"
 }
 
-variable "vm-disk" {
+# variable "vm-disk" {
+# type = string
+# description = "가상머신 Disk 사이즈"
+# default = "30"
+# }
+
+variable "vm-disk1" {
 type = string
 description = "가상머신 Disk 사이즈"
-default = "30"
+default = "0"
+}
+
+variable "vm-disk2" {
+type = string
+description = "가상머신 Disk 사이즈"
+default = "0"
+}
+
+variable "vm-disk3" {
+type = string
+description = "가상머신 Disk 사이즈"
+default = "0"
 }
 
 variable "vm-name" {
